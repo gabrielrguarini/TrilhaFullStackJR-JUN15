3533a297d8a9fc55fb7452c44fc7b72d7e405627
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-10-17 19:15:52 -0300

    feat: add verifyJWT

---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).json({ error: "Acesso negado" });
+    return;
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    res.status(401).json({ error: "Acesso negado" });
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+  req.token = decoded;
+  next();
+};
