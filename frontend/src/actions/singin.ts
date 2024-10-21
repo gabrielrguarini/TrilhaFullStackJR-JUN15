bdfd9ecb440b27f0570e0cd4ac843a7a9d21df3b
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-10-21 19:33:03 -0300

    feat: add singinSchema

--- frontend/src/actions/singin.ts
+++ frontend/src/actions/singin.ts
@@ -1,10 +1,17 @@
 "use server";
 
+import { singinSchema } from "@/schemas/singinSchema";
+
 export async function singin(formData: FormData) {
   const body = {
     email: formData.get("email"),
     password: formData.get("password"),
     repassword: formData.get("repassword"),
   };
-  console.log("formData: ", body);
+  const safeData = singinSchema.safeParse(body);
+  if (!safeData.success) {
+    console.log(safeData.error?.flatten().fieldErrors);
+    // return safeData.error?.flatten().fieldErrors;
+  }
+  console.log("Ok");
 }
--- frontend/src/schemas/singinSchema.ts
+++ frontend/src/schemas/singinSchema.ts
@@ -1,1 +1,18 @@
-export const singinSchema = z.
+import { z } from "zod";
+
+export const singinSchema = z
+  .object({
+    email: z
+      .string({ message: "Email é obrigatório" })
+      .email({ message: "Coloque um email válido" }),
+    password: z
+      .string({ message: "Email é obrigatório" })
+      .min(4, { message: "Senha de ter pelo menos 4 caracteres" }),
+    repassword: z
+      .string({ message: "Email é obrigatório" })
+      .min(4, { message: "Senha de ter pelo menos 4 caracteres" }),
+  })
+  .refine((fields) => fields.password === fields["repassword"], {
+    message: "As senhas devem ser iguais",
+    path: ["repassword"],
+  });
